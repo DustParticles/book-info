f78f2a0b9a5ae90f055fc9d252faafbb35167f41
Elkanah <elkanahawesome@gmail.com>
2022-09-02 03:15:18 -0500

    fixed svg resize when clicking read toggle  button

--- book-data.js
+++ book-data.js
@@ -74,9 +74,11 @@ function createCards() {
             <span class="info_card_text">${book.read ? "Read" : "Unread"}</span>
           </button>
       </div>
+      <div class="svg_container"> 
       <svg class="info_card" style="" viewBox="0 0 24 24">
       <path fill="currentColor" d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
        </svg></div> 
+       </div>
     
     `;
     create_Book.setAttribute("class", "card");
